ba269596b382b45b3a3189056bbe2c7b58cb3618
Paula Vasquez <avasquezc15@miumg.edu.gt>
2019-11-07 19:42:58 -0600

    Lista de Precios

---
+++ MVC_VentasyCtasporCobrar_Lista_Precios/Capa_Diseño_VentasyCtasporCobrar_lp/Lista_Precios.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Capa_Diseño_VentasyCtasporCobrar_lp
+{
+    public partial class Lista_Precios : UserControl
+    {
+        public Lista_Precios()
+        {
+            InitializeComponent();
+        }
+
+        private void Lista_Precios_Load(object sender, EventArgs e)
+        {
+            Gpb_accion.ForeColor = Color.White;
+            
+            Gpb_accion.ForeColor = Color.Black;
+        }
+    }
+}
---
+++ MVC_VentasyCtasporCobrar_Lista_Precios/Capa_Logica_VentasyCtasporCobrar_lp/logica_lista_precio.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Capa_Datos_VentasyCtasporCobrar_lp;
+
+namespace Capa_Logica_VentasyCtasporCobrar_lp
+{
+    public class logica_lista_precio
+    {
+
+    }
+}

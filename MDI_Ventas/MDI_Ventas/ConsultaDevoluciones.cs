ff4c38a787e65c8fd3adc439fe8342453ff4c030
dtorresc2 <dtorresc2@miumg.edu.gt>
2019-11-11 19:07:02 -0600

    Agregando Facturacion al MDI de Ventas

---
+++ MDI_Ventas/MDI_Ventas/ConsultaDevoluciones.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MDI_Ventas
+{
+    public partial class ConsultaDevoluciones : Form
+    {
+        public ConsultaDevoluciones()
+        {
+            InitializeComponent();
+        }
+
+        private void CU_DevolucionesConsulta1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ MDI_Ventas/MDI_Ventas/Pedidos.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace MDI_Ventas
+{
+    partial class Pedidos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cU_Pedido1 = new Facturacion.CU_Pedido();
+            this.SuspendLayout();
+            // 
+            // cU_Pedido1
+            // 
+            this.cU_Pedido1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(35)))), ((int)(((byte)(90)))));
+            this.cU_Pedido1.Location = new System.Drawing.Point(3, 1);
+            this.cU_Pedido1.Name = "cU_Pedido1";
+            this.cU_Pedido1.Size = new System.Drawing.Size(1080, 700);
+            this.cU_Pedido1.TabIndex = 0;
+            // 
+            // Pedidos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1084, 701);
+            this.Controls.Add(this.cU_Pedido1);
+            this.Name = "Pedidos";
+            this.Text = "Pedidos";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Facturacion.CU_Pedido cU_Pedido1;
+    }
+}
